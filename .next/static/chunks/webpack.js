317ddf36245720e01526882d441019c5cbedcc3e
FaisalAhmad786 <faisalahmad7868@gmail.com>
2022-06-27 13:04:32 +0530

    Added navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b442270563ddaaaf"; }
+/******/ 		__webpack_require__.h = function() { return "234c07ed90b6bed4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
